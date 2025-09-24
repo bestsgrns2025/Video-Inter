918223de23208c02de3b0352c84b30cfb758020e
bestsgrns2025 <bestsg.rns.2025@gmail.com>
2025-09-24 09:00:27 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,21 +1,9 @@
-
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    isVerified: {
-        type: Boolean,
-        default: false,
-    },
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  confirmed: { type: Boolean, default: false }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
