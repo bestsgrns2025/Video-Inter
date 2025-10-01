9ed6774b7b2682758a03fd23ad88e509d170d3e5
bestsgrns2025 <bestsg.rns.2025@gmail.com>
2025-10-01 22:18:47 +0530

    Update auth.ts

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const signup = (data: any) => {
     return axios.post(`${API_URL}/signup`, data);
